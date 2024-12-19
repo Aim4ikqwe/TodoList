1067132f062adb26b3416c1441e642f598800692
Aim4ikqwe <moykottisha2013@gmail.com>
2024-12-19 13:06:49 +0300

    Spring Security

---
+++ src/main/java/com/example/todoapp/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.todoapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+@Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+    UserDetails user = User.builder().username("user").password(encoder.encode("user")).build();
+
+    return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/register/**").permitAll()
+                    .requestMatchers("/").permitAll()
+                    .requestMatchers("/style.css/**").permitAll()
+                    .requestMatchers("/**").authenticated())
+            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // TODO 16:30 Доделать авторизацию и регистрацию
+            .build();
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/todoapp/model/Identification.java
+++ src/main/java/com/example/todoapp/model/Identification.java
@@ -3,9 +3,14 @@ package com.example.todoapp.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Data
+@Builder
 public class Identification {
     @Id
     @GeneratedValue
@@ -13,36 +18,4 @@ public class Identification {
     private String Login;
     private String Password;
 
-    public Identification() {
-    }
-
-    public Identification(long id, String login, String password) {
-        this.id = id;
-        Login = login;
-        Password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return Login;
-    }
-
-    public void setLogin(String login) {
-        Login = login;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
 }
